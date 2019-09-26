a1da68786f2811602c81f333489e757094fee373
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-09-26 16:49:04 +0200

    Ejercicio 3 - Skins

---
+++ Ejercicio 3 - Selector de skins.php
@@ -0,0 +1,14 @@
+<!-- Ejercicio 3 forms - Ferran Herrero 26/09/2019 -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Selector de skins - Ferran H</title>
+    <link rel="stylesheet" href="/Ex3_Skins.css/ex3.css">
+  </head>
+  <body>
+      <h1>Hola mundo!</h1>
+      <p>Este es el ejercicio 3 sobre lectura de directorios realizado por Ferran Herrero.</p>
+
+  </body>
+</html>
