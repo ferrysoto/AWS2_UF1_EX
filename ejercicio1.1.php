3b8950b3c52087794c4c9854251721eb33e21af7
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-09-26 16:28:18 +0200

    Ejercicio 1.1 - Formularios

---
+++ ejercicio1.1.php
@@ -0,0 +1,21 @@
+<!-- Ejercicio 1.1- Ferran Herrero 20/09/2019 -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hello world</title>
+	</head>
+	<body>
+		<?php  
+			// declarar variables
+			$backgroundColor = "tomato";
+			$linksColor = "#00FF00";
+			$letterColor = "#FF5A14";
+			$H1Color = "green";
+			$message = "Aprenem PHP com qui no vol la cosa.";
+
+			// mostrar por pantalla
+			echo "<p><font color='" . $letterColor ."'> " . $message . " </font></p>";
+
+		?>
+	</body>
+</html>
