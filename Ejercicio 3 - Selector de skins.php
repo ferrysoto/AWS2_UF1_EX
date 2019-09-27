a0300b83142b9f266018a4e69999cbb7582e5f9d
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-09-27 17:40:27 +0200

    Ejercicio 3 - Mejorado

--- Ejercicio 3 - Selector de skins.php
+++ Ejercicio 3 - Selector de skins.php
@@ -4,7 +4,13 @@
   <head>
     <meta charset="utf-8">
     <title>Selector de skins - Ferran H</title>
-    <link rel="stylesheet" href="/Ex3_Skins.css/ex3_default.css">
+    <?php
+      if (empty($_POST)) {
+        echo '<link rel="stylesheet" href="/Ex3_Skins/ex3_default.css">';
+      } else {
+        echo '<link rel="stylesheet" href="/Ex3_Skins/'. $_POST["selectSkin"].'"/>';
+      } ?>
+
   </head>
   <body>
     <div style="width: 100%; padding: 15px;">
@@ -16,10 +22,10 @@
 
         <form style="padding-top: 10px; padding-right: 35px;" method="post">
           <label>Elige una skin: </label>
-          <select name="selectSkin">
-            <option value="ex3_default.css">Skin default</option>
-            <option value="ex3_v1.css">Skin 1</option>
-            <option value="ex3_v2.css">Skin 2</option>
+          <select name="selectSkin" action="Ejercicio 3 - Selector de skins.php">
+            <option>Skin default</option>
+            <option>Skin 1</option>
+            <option>Skin 2</option>
           </select>
           <input type="submit">
         </form>
@@ -35,10 +41,8 @@
       </div>
     </div>
 
-    <link rel="stylesheet" href="/Ex3_Skins.css/<?php echo $skin ?>">
     <p>Este es el ejercicio 3 sobre lectura de directorios realizado por Ferran Herrero.</p>
     <br>
-    <strong>Skin seleccionada: <?php echo $skin ?></strong>
 
   </body>
 </html>
