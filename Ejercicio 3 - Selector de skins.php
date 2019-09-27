a41887397141a746a81601d3f66aa6359fec77ab
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-09-27 17:44:47 +0200

    Ejercicio 3 - Mejorado

--- Ejercicio 3 - Selector de skins.php
+++ Ejercicio 3 - Selector de skins.php
@@ -23,9 +23,9 @@
         <form style="padding-top: 10px; padding-right: 35px;" method="post">
           <label>Elige una skin: </label>
           <select name="selectSkin" action="Ejercicio 3 - Selector de skins.php">
-            <option>Skin default</option>
-            <option>Skin 1</option>
-            <option>Skin 2</option>
+            <option value="ex3_default.css">Skin default</option>
+            <option value="ex3_v1.css">Skin 1</option>
+            <option value="ex3_v2.css">Skin 2</option>
           </select>
           <input type="submit">
         </form>
